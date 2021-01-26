b3a4cac13761de543bb2d3f9cc4ee67049590c6f
Edvins Antonovs <ummahusla@gmail.com>
2021-01-26 11:54:56 +0200

    Finished implementation

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {combineReducers} from "redux";
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
 import counterReducer from '../features/counter/counterSlice';
 
-export default configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+const reducers = combineReducers({
+  counter: counterReducer
 });
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: [thunk]
+});
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+          <App/>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
